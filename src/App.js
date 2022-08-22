3ab95a8d12e5b24cd83a93ccc266b8adc39a5a69
Shahim <sehemsahin1989@hotmail.com>
2022-08-22 17:10:36 +0300

    Motion added to the Counter

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import {Button, ButtonGroup} from '@mui/material';
 import {AddCircle, Send, RemoveCircle} from '@mui/icons-material';
+import {motion} from 'framer-motion'
 
 
 
@@ -11,18 +12,51 @@ const App = () => {
 
 
 const [count,setCount] = useState(0);
+const [animate,setAnimate] =useState('');
+const [transition,setTransition] =useState('');
+// const [initial, setInitial] = useState('')
 
 function countAdd(){
+          // setInitial({opacity:0.4})
+          setAnimate({y:0 })
+          setTransition({from:-30, duration:0.5, type:'spring', bounce:0.5})
+
+          setTimeout(()=>{
+            // setInitial('')
+            setAnimate('')
+            setTransition('')
+          },500)
+
   return setCount(count+1)
 }
 function countSubtract(){
+          // setInitial({opacity:1})
+          setAnimate({y:0})
+          setTransition({from:30, duration:0.5, type:'spring', bounce:0.5})
+
+          setTimeout(()=>{
+            // setInitial('')
+            setAnimate('')
+            setTransition('')
+          },500)
+
   return setCount(count-1)
 }
+
+
+useEffect( () => {
+
+  console.log('Value Changed!😂😂🤣')
+ 
+
+},[count])
+
+
 
 return(
     <div className="App">
       <h1>Hello</h1>
-      <h1>{count}</h1>
+      <motion.h1  animate={animate} transition={transition} >{count}</motion.h1>
 <ButtonGroup variant="contained" aria-label='outlined primary'>
       <Button variant='contained' size="large" onClick={countAdd} startIcon={<AddCircle />}>Add</Button>
       <Button variant='contained' size="large" onClick={countSubtract} startIcon={<RemoveCircle />}>Subtract </Button>
