0c2ff4777d906314a74754f362c59d605bfe1e06
Shahim <sehemsahin1989@hotmail.com>
2022-08-22 17:38:17 +0300

    better animation

--- src/App.js
+++ src/App.js
@@ -19,26 +19,26 @@ const [transition,setTransition] =useState('');
 function countAdd(){
           // setInitial({opacity:0.4})
           setAnimate({y:0 })
-          setTransition({from:-30, duration:0.5, type:'spring', bounce:0.5})
+          setTransition({from:-30, duration:0.4, type:'spring', bounce:0.5})
 
           setTimeout(()=>{
             // setInitial('')
             setAnimate('')
             setTransition('')
-          },500)
+          },100)
 
   return setCount(count+1)
 }
 function countSubtract(){
           // setInitial({opacity:1})
           setAnimate({y:0})
-          setTransition({from:30, duration:0.5, type:'spring', bounce:0.5})
+          setTransition({from:30, duration:0.4, type:'spring', bounce:0.5})
 
           setTimeout(()=>{
             // setInitial('')
             setAnimate('')
             setTransition('')
-          },500)
+          },100)
 
   return setCount(count-1)
 }
@@ -59,7 +59,7 @@ return(
       <motion.h1  animate={animate} transition={transition} >{count}</motion.h1>
 <ButtonGroup variant="contained" aria-label='outlined primary'>
       <Button variant='contained' size="large" onClick={countAdd} startIcon={<AddCircle />}>Add</Button>
-      <Button variant='contained' size="large" onClick={countSubtract} startIcon={<RemoveCircle />}>Subtract </Button>
+      <Button variant='contained'color="secondary" size="large" onClick={countSubtract} startIcon={<RemoveCircle />}>Subtract </Button>
       </ButtonGroup>
 
     </div>
